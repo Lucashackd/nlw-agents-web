ddad6e4b0e68d1163bd828bddf9c6abb2e800002
Lucashackd <lucashackd@gmail.com>
2025-07-13 23:36:49 -0300

    feat: add create question, record audio

---
+++ src/http/use-create-question.ts
@@ -0,0 +1,29 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import type { CreateQuestionRequest } from './types/create-question-request';
+import type { CreateQuestionResponse } from './types/create-question-response';
+
+export function UseCreateQuestion(roomId: string) {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (data: CreateQuestionRequest) => {
+      const response = await fetch(
+        `http://localhost:3333/rooms/${roomId}/questions`,
+        {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      const result: CreateQuestionResponse = await response.json();
+
+      return result;
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['get-questions', roomId] });
+    },
+  });
+}
